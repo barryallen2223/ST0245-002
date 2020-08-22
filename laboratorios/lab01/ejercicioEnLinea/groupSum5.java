8d96731a2b16593bae579068cc0ca77e0f41dedc
dmadridr <68928426+dmadridr@users.noreply.github.com>
2020-08-22 00:53:30 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/groupSum5.java
@@ -0,0 +1,23 @@
+
+public class groupSum5
+{
+    public boolean groupSum5(int start, int[] nums, int target) {
+  
+      if(start == nums.length){
+        return (target == 0);
+      }
+      
+      if(nums[start]%5 == 0){
+        if(start < nums.length-1){
+          if(nums[start+1] == 1)
+          return groupSum5(start+2, nums, target-nums[start]);
+        }
+        return groupSum5(start+1, nums, target-nums[start]);
+      }
+      
+      if(target == 0)
+      return true;
+      
+      return groupSum5(start+1, nums, target-nums[start]) || groupSum5(start+1, nums, target);
+    }
+}
