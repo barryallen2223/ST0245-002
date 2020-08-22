a96d42fc2e2c0b5699b52747727cb1b9105149e7
dmadridr <68928426+dmadridr@users.noreply.github.com>
2020-08-22 18:21:39 -0500

    Update FillRectangles.java

--- laboratorios/lab01/codigo/Java Language/FillRectangles.java
+++ laboratorios/lab01/codigo/Java Language/FillRectangles.java
@@ -1,12 +1,11 @@
 public class FillRectangles{
     
     public double fillRectangle(int n){
-        return Math.pow(auxFill(n, 0),2)-1;
+        if(n > 1)
+        {
+            return fillRectangle(n-2) + fillRectangle(n-1);
+        }
+        return 1;
     }
     
-    public int auxFill(int n, int cont){
-        if(n > 1)
-        return auxFill(n-2, cont) + auxFill(n-1, cont);
-        return ++cont;
-    }
 }
