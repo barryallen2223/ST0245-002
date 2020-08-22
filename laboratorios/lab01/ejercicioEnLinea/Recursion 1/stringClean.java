42b231015662649a5bcccda0523485350561d288
dmadridr <68928426+dmadridr@users.noreply.github.com>
2020-08-22 01:23:18 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/Recursion 1/stringClean.java
@@ -0,0 +1,15 @@
+
+public class stringClean
+{
+    public String stringClean(String str) {
+      if(str.length() < 2) return str;
+      if(str.charAt(0) == str.charAt(1)){
+        return stringClean(str.substring(1));
+      }
+      else {
+        return str.charAt(0) + stringClean(str.substring(1));
+      }
+        
+    }
+
+}
