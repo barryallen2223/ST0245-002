481047d5045164a3c44bbf1e9d0e601f57560176
salvarezo1 <68928344+salvarezo1@users.noreply.github.com>
2020-09-03 20:37:43 -0500

    Update maxSpan.java

--- laboratorios/lab02/ejercicioEnLinea/Array3/maxSpan.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3/maxSpan.java
@@ -1,15 +1,21 @@
 public int maxSpan(int[] nums) {
-  int cont=0;
-  if(nums.length==0)return 0;
-  if(nums[0]!=nums[nums.length-1]){
-    for(int i = 1 ; i < nums.length ; i++){
-      cont++;
+  return this.saveLast(nums, new int[2]);
+}
+
+public int saveLast(int[] nums, int[] cof){
+  
+  for(int pos = 0; pos < nums.length - cof[0]; pos++){
+    
+    if(nums[pos] == cof[1])
+      continue;
+    
+    for(int n = nums.length-1; n >= pos; n--){
+      if(nums[pos] == nums[n]){
+        cof[0] = Math.max(n - pos + 1, cof[0]);
+        cof[1] = (cof[0] == n - pos + 1) ? n : cof[1];
+      }
     }
+    
   }
-  else if(nums[0]==nums[nums.length-1]){
-    for(int i = 0 ; i < nums.length ; i++){
-      cont++;
-    }
-  }
-  return cont;
+  return cof[0];
 }
