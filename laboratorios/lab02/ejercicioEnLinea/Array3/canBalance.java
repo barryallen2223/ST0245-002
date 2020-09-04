ec478a5ad3361533a0f4e2fc1ce90a0819c38bfd
dmadridr <68928426+dmadridr@users.noreply.github.com>
2020-09-03 20:06:31 -0500

    Add files via upload

---
+++ laboratorios/lab02/ejercicioEnLinea/Array3/canBalance.java
@@ -0,0 +1,20 @@
+
+public class canBalance
+{
+    public boolean canBalance(int[] nums) {
+        int sum1=0, sum2=0;
+        if(nums.length==1){
+            return false;
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            sum1+= nums[i];
+            sum2=0;
+            for(int j = nums.length-1 ; j > i ; j--){
+                sum2+= nums[j];
+            }
+            if(sum1==sum2)return true;
+        }
+        return false;
+    }
+
+}
---
+++ laboratorios/lab02/ejercicioEnLinea/Array3/maxSpan.java
@@ -0,0 +1,20 @@
+
+public class maxSpan
+{
+    public int maxSpan(int[] nums) {
+        int cont=0;
+        if(nums.length==0)return 0;
+        if(nums[0]!=nums[nums.length-1]){
+            for(int i = 1 ; i < nums.length ; i++){
+                cont++;
+            }
+        }
+        else if(nums[0]==nums[nums.length-1]){
+            for(int i = 0 ; i < nums.length ; i++){
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+}
