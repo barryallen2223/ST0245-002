6dea5c1f6aec36b8ee2ddf0dd7701bbc660375b8
salvarezo1 <68928344+salvarezo1@users.noreply.github.com>
2020-10-16 17:05:15 -0500

    Add files via upload

---
+++ talleres/taller10/Java Language/Node.java
@@ -0,0 +1,46 @@
+public class Node
+{
+    Object data;
+    Node next;
+    
+    public Node(){
+        this.data = null;
+        this.next = null;
+    }
+    
+    public Node(Object data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    public Node(Object data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    public void setData(Object data){
+        this.data = data;
+    }
+    
+    public Object getData(){
+        return data;
+    }
+    
+    public void setNext(Node next){
+        this.next = next;
+    }
+    
+    public Node getNext(){
+        return next;
+    }
+    
+    public int size(Node headNode){
+        int size = 0;
+        Node currentNode = headNode;
+        while(currentNode != null){
+            size++;
+            currentNode = currentNode.next;
+        }
+        return size;
+    }
+}
