ee7bb69075799b02658563b8d81cf0124de60504
salvarezo1 <68928344+salvarezo1@users.noreply.github.com>
2020-11-07 02:41:20 -0500

    Add files via upload

--- laboratorios/lab05/codigo/TwoColors.java
+++ laboratorios/lab05/codigo/TwoColors.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Write a description of class TwoColors here.
@@ -8,6 +9,54 @@
 public class TwoColors
 {
     public boolean isColorable(DigraphAL graph){
-        return false;
+        Node Start = graph.getHead(0);
+        
+        ArrayList<Node> blue = new ArrayList<>();
+        ArrayList<Node> red = new ArrayList<>();
+        
+        blue.add(Start);
+        
+        for(int i = 0; i < graph.size(); i++){
+            for(Node j : graph.getSuccessors(i)){
+                if(blue.contains(j)){
+                    if(red.contains(j))
+                        return false;
+                    red.add(j);
+                } else if(red.contains(j)){
+                    if(blue.contains(j))
+                        return false;
+                    blue.add(j);
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args){
+        TwoColors t = new TwoColors();
+        DigraphAL graph = new DigraphAL(5);
+        
+        Node a0 = new Node((Integer) 0);
+        Node a1 = new Node((Integer) 1);
+        Node a2 = new Node((Integer) 2);
+        Node a3 = new Node((Integer) 3);
+        Node a4 = new Node((Integer) 4);
+        
+        graph.setHead(0, a0);
+        graph.setHead(1, a1);
+        graph.setHead(2, a2);
+        graph.setHead(3, a3);
+        graph.setHead(4, a4);
+        
+        graph.addArc(a0, a4, 4.0);
+        graph.addArc(a0, a1, 3.0);
+        graph.addArc(a1, a2, 2.0);
+        graph.addArc(a1, a3, 5.0);
+        graph.addArc(a0, a2, 6.0);
+        graph.addArc(a2, a1, 2.0);
+        graph.addArc(a2, a4, 5.0);
+        
+        System.out.println(t.isColorable(graph));
     }
 }
